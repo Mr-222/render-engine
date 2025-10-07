cf5c2eb90f3d7a1a6ed6f58bd502019e5971eac0
Yutong Sun <ysun730@gatech.edu>
2025-10-07 14:23:37 -0400

    Fix compilate issue in release mode.

--- shader_target.lua
+++ shader_target.lua
@@ -10,21 +10,4 @@ function shader_target(name)
     add_files("**/node/" .. name .. "/*.vert")
     add_files("**/node/" .. name .. "/*.frag")
     add_files("**/node/" .. name .. "/*.geom")
-    after_build(function(target)
-        if not is_mode("release") then
-            return
-        end
-        import("lib.detect.find_program")
-        local spirv_opt = find_program("spirv-opt")
-        if spirv_opt == nil then
-            print("spirv-opt not found, no shader optimization will be performed")
-            return
-        end
-        local folder = "$(builddir)/shaders/" .. name
-        for _, filepath in ipairs(os.files(folder .. "/*.spv")) do
-            os.runv(spirv_opt, { filepath, "-o", filepath .. ".opt", "-O" })
-            os.rm(filepath)
-            os.mv(filepath .. ".opt", filepath)
-        end
-    end)
 end
